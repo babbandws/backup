3fecd4259bf1bfa1d31e31013d692522c4664362
babbandws <babbandeep@digitalwebsolutions.in>
2017-01-18 12:56:15 +0530

    Create index.php

---
+++ zipperties/index.php
@@ -0,0 +1,469 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="robots" content="noodp"/>
+<meta name="google-site-verification" content="pcDi0UyIQjsOGzug2g1TG-mC9_Bz-9alBkKv1jbrfec" />
+
+				  
+
+<meta name="keywords" content="australian zipper ties, school ties,boys zipper ties, zipper ties, school uniform ties, Zip neck tie" />
+<meta name="description" content="Tie knotting has now got much easier and exciting with Australian zipper ties. Get zip neck boys school uniform ties for you right from us - Australian zipper ties." />
+
+
+<title>School Uniform Ties | Boys Zipper Ties - Australian Zipper Ties</title>
+
+
+<!-- Meta HTML -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Style HTML -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/screen.css">
+	<link rel="stylesheet" type="text/css" href="css/text.css">
+
+	<!-- Javascript Required --> 
+	<script type="text/javascript" src="js/jquery.min.js"></script> 
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/respond.matchmedia.addListener.min.js"></script> 
+	<script type="text/javascript" src="js/modernizr.custom.js"></script>
+	
+
+	<!--[if IE]>
+	<style type="text/css">
+		<style type="text/css">img, div, a, input { behavior: url('assets/iepngfix.htc'); }</style> 
+	<![endif]-->
+	
+<script type="text/javascript">
+// searchbox functions ( clear &amp; unclear )
+  
+      function clickclear(thisfield, defaulttext) {
+  
+      if (thisfield.value == defaulttext) {
+ 
+      thisfield.value = "";
+      }
+   
+      }
+
+      function clickrecall(thisfield, defaulttext) {
+ 
+      if (thisfield.value == "") {
+
+      thisfield.value = defaulttext;
+
+      }
+
+      }
+</script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18315606-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<link rel="shortcut icon" href="http://www.zipperties.com.au/images/favicon.ico" type="image/x-icon"/>
+<link rel="icon" href="http://www.zipperties.com.au/images/favicon.ico" type="image/x-icon"/>
+
+</head>
+
+<body>
+
+
+
+<div class="wrapper">
+
+
+
+	<!--  BEGIN OF TOP BAR  -->
+	<div class="topBar">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+				  	<ul>
+				       <!-- <li><a href="http://www.zipperties.com.au/signin.php">SIGN IN</a> |</li>
+				        <li><a href="http://www.zipperties.com.au/signup.php">SIGN UP</a></li>-->
+				        <li class="chart"><a href="mycart.php"><i class="icon iconChart"></i> QUOTE CART (0 ITEMS)</a></li>
+				     </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--  END OF TOP BAR  -->
+
+
+
+
+
+<!-- BEGIN OF HEADER -->
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 logo">
+					<a href="http://www.zipperties.com.au/"><img src="images/logo.png"></a>
+				</div>
+				<div class="col-sm-8">
+					<div class="row">
+						<div class="col-sm-8">
+							<!--<form class="form-inline boxSearch" role="form" action="/mysearch.php">
+							  	<div class="form-group">
+							    	<label class="sr-only" for="search">Search</label>
+							    	<input type="text" class="form-control" id="myinput" placeholder="Search" name="myinput">
+							  	</div>
+							  	<div class="form-group">
+							    
+									  
+             					 <select name="mycolour" class="form-control">
+
+          
+                        
+                                  <option value="all">All colours</option>
+                        
+                                  <option value="beige_brown" style="color:#996633; background-color:#996633;">BROWN/BEIGE</option>
+                        
+                                 <option value="black_grey" style="color:#000000; background-color:#000000;">BLACK/GREY</option>
+                        
+                                  
+                        
+                                  <option value="white_stone" style="color:#ffffff; background-color:#ffffff;">WHITE/STONE</option> 
+                        
+                                  <option value="yellow_orange" style="color:#FFCC00; background-color:#FFCC00;">YLW/ORANGE</option> 
+                        
+                                
+                        
+                                  <option value="red_pink" style="color:#FF0000; background-color:#FF0000;">RED/PINK</option>  
+                        
+                                  <option value="blue_purple" style="color:#0066FF; background-color:#0066FF;">BLUE/PURPLE</option>
+                        
+                                  <option value="green_teal" style="color:#66CC00; background-color:#66CC00;">GREEN/TEAL</option>
+                    
+                              </select>
+
+							  	</div>
+							  	<div class="form-group">
+							    
+									     <select name="mygender" class="form-control">
+
+                                              <option value="all">All genders</option>
+                                    
+                                              <option value="ladies">LADIES</option>
+                                    
+                                              <option value="mens">MENS</option>
+                                    
+                                              <option value="unisex">UNISEX</option>
+                                
+                                          </select>
+
+							  	</div>
+							  	<button type="submit" class="btn btn-default"  onclick="submit();">OK</button>
+							</form>-->
+						</div>
+						<div class="col-sm-4 areaNumber">
+							<!--<p class="phoneNumber">PREFER TO <span>TALK?</span></p>-->
+							<p class="blockNumber">
+								<!--<i class="icon iconChat"></i>-->
+								1800 815 571<br />(07) 3267 2850 
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</header>
+	<!-- END OF HEADER -->
+
+    
+
+     
+<!--  BEGIN OF MENU  -->
+	<div class="menu">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<nav class="navbar navbar-default" role="navigation">
+					  	<div class="navbar-header">
+					    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#welborneMenu">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+					    	</button>
+					    	<a class="navbar-brand" href="#" data-target="#welborneMenu" data-toggle="collapse" >Menu</a>
+					  	</div>
+					  	<div class="collapse navbar-collapse" id="welborneMenu">
+					    	<ul class="nav navbar-nav">
+						      	<!--<li class="active"><a href="#">Home</a></li>                  -->
+						      	  
+    
+    <li class="active"><a href="/index.php" >HOME </a></li><li class=""><a href="/about-us.html" >ABOUT US </a></li><li class=""><a href="/prices.html" >PRICES </a></li><li class=""><a href="/index.php?content=129" >PRODUCTS </a></li><li class="dropdown"><a href="/#" >INFO </a><ul class="sub-menu"><li><a href="/freight-charges.html">Freight Charges</a></li><li><a href="/exchanges-returns.html">Exchanges / Returns</a></li><li><a href="/downloads.html">Downloads</a></li></ul></li><li class=""><a href="/index.php?content=131" >LINKS </a></li><li class=""><a href="/index.php?content=132" >BLOG </a></li>	   
+     
+     
+	
+
+     <li><a href="contact_wi.php">CONTACT</a></li> 	
+     
+    <!-- <li><a href="signin.php">CLIENT LOGIN</a></li> 		-->
+                  
+
+
+
+
+					   		</ul>
+					  	</div>
+					</nav>
+				</div>
+			</div>
+		</div>		
+	</div>
+	<!--  END OF MENU  -->
+
+    
+
+  
+
+
+	<!--  BEGIN OF SLIDER  -->
+	<div class="slideshow">
+		<div id="welborneSlider" class="carousel slide" data-ride="carousel">
+		  	<!-- Wrapper for slides -->
+		  	<div class="carousel-inner">
+            
+                        
+		    	<div class="item active">
+			      	<img src="images/uploads/126498966.jpg" alt="">
+			      	<div class="carousel-caption">
+			        	<p><br/>
+			        		<span class="orange"></span>
+			        		
+			        				      	</div>
+		    	</div>
+                
+                
+                            
+		    	<div class="item ">
+			      	<img src="images/uploads/147059343.jpg" alt="">
+			      	<div class="carousel-caption">
+			        	<p><br/>
+			        		<span class="orange"></span>
+			        		
+			        				      	</div>
+		    	</div>
+                
+                
+                            
+		    	<div class="item ">
+			      	<img src="images/uploads/314167106.jpg" alt="">
+			      	<div class="carousel-caption">
+			        	<p><br/>
+			        		<span class="orange"></span>
+			        		
+			        				      	</div>
+		    	</div>
+                
+                
+                                
+		    	<!--<div class="item">
+			      	<img src="images/slider1.jpg" alt="">
+			      	<div class="carousel-caption">
+			        	<p>HOME APPLIANCES<br/>
+			        		<span class="orange">Save Up To</span><br/>
+			        		<span class="red big">15% OFF</span></p>
+			        	<p><a href="product.html" class="btn btn-default">SHOP NOW</a></p>
+			      	</div>
+		    	</div>-->
+		    	
+                
+                
+                
+                
+                
+		  	</div>
+		  	<!-- Controls -->
+  			<a class="left carousel-control" href="#welborneSlider" data-slide="prev">
+    			<i class="icon iconNavLeft"></i>
+  			</a>
+  			<a class="right carousel-control" href="#welborneSlider" data-slide="next">
+    			<i class="icon iconNavRight"></i>
+  			</a>
+		</div>
+	</div>
+	<!--  END OF SLIDER  -->
+	
+    
+
+  <!-- BEGIN OF CONTENT -->
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 center subText">
+					
+                    
+                    
+                    
+                    
+                    <h1>Welcome to Australian Zipper Ties - Zip Neck Tie</h1>
+					<hr/>
+                    
+                    
+   					     
+           
+      
+        
+ <div><h2>What is a Zipper tie?</h2><p>A zipper tie is essentially a regular tie with a permanent knot and a zipper mechanism inside it, which allows you to adjust the width of the neck accordingly; hence, a great alternative to regular school ties.<br />&nbsp;</p><p><strong>Adjusting zipper ties is an easy process:-</strong></p><p>Slip the zip neck tie over your head.</p><p>Take hold of the inner tail of the tie and the knot.</p><p>Once you have done so, you can slide the zipper up, by holding the tail firmly and sliding the knot up to a comfortable position at your neck.</p><p>With the tie in place, the zipper will remain in that fixed position with an easily controllable yet firm grip.</p><p>You always have a perfect knot with Australian Zipper ties.</p><p>&nbsp;</p><p><strong>Zipper Tie benefits:-</strong></p><ul>	<li>A true looking Windsor knot</li>	<li>It&rsquo;s easy on and easy off design</li>	<li>Perfect length every time</li>	<li>New &amp; Improved non-locking slider mechanism</li>	<li>Strong and durable</li>	<li>Time saver for parents and children</li>	<li>These custom made ties are easy for people with disabilities or reduced movement in their hands</li></ul><p>Australian Zipper Ties are available in 4 lengths, from X-Small &ndash; X-Large providing the right length for all wearers from children to adults. Specifically designed school uniform ties are available with us.</p><div class="row"><div class="col-sm-12"><h2><em>OUR OTHER DIVISIONS</em></h2></div></div><em><div class="row" style="text-align:center"><div class="col-sm-2"></div><div class="col-sm-4"><a href="http://theuniformcompany.com.au/" target="_blank" rel="nofollow"><div class="productList p4"><p>&nbsp;</p></div></a></div><div class="col-sm-4"><a href="http://welbornecorporateimage.com.au/" target="_blank" rel="nofollow"><div class="productList p5"></div></a></div></div></em><p>&nbsp;</p></div>        
+        
+        
+        
+        
+                  
+         
+                    
+        
+              
+      
+        
+              
+      
+      	 
+	  <hr/>
+				</div>
+			</div>
+            
+            
+            
+            
+            
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="box">
+						<div class="labelTitle">Our Testimonials - <a href="testimonials.html" style="color:#FFF">View All</a></div>
+						<div class="boxBody">
+                        
+                    					
+                        
+							<div class="media quote">
+								<i class="icon iconQuote pull-left"></i>
+								<div class="media-body">
+								    <p class="testimonial">Williamson International has been supplying uniforms to our High School for many years. In my 6 year association with Williamsons ...</p>
+									<p class="name">Kenmore State High School</p>
+								</div>
+							</div>
+                            
+                            
+                            
+                            
+							<!--<div class="media quote">
+								<i class="icon iconQuote pull-left"></i>
+								<div class="media-body">
+								    <p class="testimonial">Proin nec tempor urna. Nulla facilisi. Vestibulum sollicitudin, risus at tempus elementum, enim lacus dignissim mauris</p>
+									<p class="name">Michael Moore</p>
+								</div>
+							</div>-->
+						</div>
+					</div>
+				</div>
+				<!--<div class="col-sm-4">
+					<div class="box">
+												
+						<div class="labelTitle">Social networking</div>
+						<div class="boxBody">
+							<ul class="social">
+								<li><a href="https://www.facebook.com/WelborneCorporateImage" target="_blank"><i class="icon iconSocial iconFacebook"></i></a></li>
+								<li><a href="https://twitter.com/johnwelborne" target="_blank"><i class="icon iconSocial iconTwitter"></i></a></li>
+								<li><a href="http://au.linkedin.com/pub/welborne-corporate-image/4b/a67/991" target="_blank"><i class="icon iconSocial iconLinkedIn"></i></a></li>
+							</ul>
+                            
+                            
+                            
+                            
+						</div>	
+					</div>
+				</div>-->
+				
+        
+        
+        
+        
+                <div style="clear:both;"><hr /></div>
+                
+                
+		            
+            
+                         
+	
+   
+		</div>
+	</div>
+	<!-- END OF CONTENT	-->
+    
+    <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
+<script type="text/javascript" src="http://arrow.scrolltotop.com/arrow79.js"></script>
+<noscript>Not seeing a <a href="http://www.scrolltotop.com/">Scroll to Top Button</a>? Go to our FAQ page for more info.</noscript>
+
+
+
+   
+	<!-- BEGIN OF FOOTER -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row">
+						<div class="col-sm-12" style="text-align:center;">
+							
+							<p class="center" style="color:#666">	 
+    
+    <a href="/index.php" >HOME </a> &nbsp; | &nbsp;<a href="/about-us.html" >ABOUT US </a> &nbsp; | &nbsp;<a href="/prices.html" >PRICES </a> &nbsp; | &nbsp;<a href="/index.php?content=129" >PRODUCTS </a> &nbsp; | &nbsp;<a href="/#" >INFO </a> &nbsp; | &nbsp;<a href="/index.php?content=131" >LINKS </a> &nbsp; | &nbsp;<a href="/index.php?content=132" >BLOG </a> &nbsp; | &nbsp;	   
+     
+     
+	
+
+    <a href="/contact_wi.php">CONTACT</a> &nbsp; | &nbsp; 	
+     
+    </p>
+							
+						</div>
+						
+					</div>
+				</div>
+				
+			</div>	
+		</div>	
+	</footer>
+	<!-- END OF FOOTER -->
+
+
+
+	<!-- BEGIN OF FOOTER BOTTOM -->
+	<div class="footerBar">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+<p><a href="https://www.facebook.com/login"><img src="images/facebook.png" width='28' height='28' alt="facebook" /></a>
+<a href="https://www.linkedin.com/uas/login"><img src="images/LinkedIn.png" width='28' height='28' alt="linkedin" /></a>
+<a href="https://plus.google.com/"><img src="images/googleplus.png" width='28' height='28' alt="googleplus" /></a>
+<a href="https://twitter.com/login"><img src="images/Twitter.png" width='28' height='28' alt="twitter" /></a></p>
+					<p class="center">&copy; 2017 Australian Zipper Ties. All Rights Reserved :: <a href="admin/" target="_blank">ADMIN</a> :: Web Design Brisbane by <a href="http://www.caboodleweb.com/" rel="nofollow">Caboodle Web Design</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END OF FOOTER BOTTOM -->
+
+
+
+</div>	
+</body>
+</html>	
+
